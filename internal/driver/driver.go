33698bfa02d75096563c58630a27410bcb0d0efd
Din Music <din.music@canonical.com>
2025-06-13 14:38:55 +0000

    internal/driver: Add Create/Delete volume controller capability
    Signed-off-by: Din Music <din.music@canonical.com>

--- internal/driver/driver.go
+++ internal/driver/driver.go
@@ -97,6 +97,10 @@ func NewDriver(opts DriverOptions) *Driver {
 		nodeID:         opts.NodeID,
 	}
 
+	d.SetControllerServiceCapabilities(
+		csi.ControllerServiceCapability_RPC_CREATE_DELETE_VOLUME,
+	)
+
 	return d
 }
 
