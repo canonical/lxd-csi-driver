d34ec6666f997b70c1454d767f4dde437224918c
Din Music <din.music@canonical.com>
2025-06-13 12:08:16 +0000

    internal/driver: Scaffolding for CSI servers
    Signed-off-by: Din Music <din.music@canonical.com>

---
+++ internal/driver/identity.go
@@ -0,0 +1,19 @@
+package driver
+
+import (
+	"github.com/container-storage-interface/spec/lib/go/csi"
+)
+
+type identityServer struct {
+	driver *Driver
+
+	// Must be embedded for forward compatibility.
+	csi.UnimplementedIdentityServer
+}
+
+// NewIdentityServer returns a new instance of the CSI identity server.
+func NewIdentityServer(driver *Driver) *identityServer {
+	return &identityServer{
+		driver: driver,
+	}
+}
---
+++ internal/driver/node.go
@@ -0,0 +1,19 @@
+package driver
+
+import (
+	"github.com/container-storage-interface/spec/lib/go/csi"
+)
+
+type nodeServer struct {
+	driver *Driver
+
+	// Must be embedded for forward compatibility.
+	csi.UnimplementedNodeServer
+}
+
+// NewNodeServer returns a new instance of the CSI node server.
+func NewNodeServer(driver *Driver) *nodeServer {
+	return &nodeServer{
+		driver: driver,
+	}
+}
