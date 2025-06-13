73b380be51ca6c7ad8dbb47e3739cdc2b39e997e
Din Music <din.music@canonical.com>
2025-06-13 14:13:15 +0000

    internal/driver: Add constant where CSI mounts filesystem volumes
    Signed-off-by: Din Music <din.music@canonical.com>

--- internal/driver/driver.go
+++ internal/driver/driver.go
@@ -19,6 +19,10 @@ import (
 // It is set during the build.
 const driverVersion = "dev"
 
+// driverFileSystemMountPath is the path where the CSI driver mounts
+// the filesystem volumes.
+const driverFileSystemMountPath = "/mnt/lxd-csi"
+
 // Default CSI driver configuration values.
 const (
 	DefaultDriverName     = "lxd.csi.canonical.com"
