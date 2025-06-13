13abd5d0402e00208942a53acb4039598d22dcf8
Din Music <din.music@canonical.com>
2025-06-13 12:39:53 +0000

    internal/driver: Setup identity server
    Signed-off-by: Din Music <din.music@canonical.com>

--- internal/driver/identity.go
+++ internal/driver/identity.go
@@ -1,7 +1,12 @@
 package driver
 
 import (
+	"context"
+
 	"github.com/container-storage-interface/spec/lib/go/csi"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/wrapperspb"
 )
 
 type identityServer struct {
@@ -16,4 +21,41 @@ func NewIdentityServer(driver *Driver) *identityServer {
 	return &identityServer{
 		driver: driver,
 	}
+}
+
+func (i *identityServer) GetPluginInfo(ctx context.Context, req *csi.GetPluginInfoRequest) (*csi.GetPluginInfoResponse, error) {
+	if i.driver.name == "" {
+		return nil, status.Error(codes.Unavailable, "Driver is missing name")
+	}
+
+	if i.driver.version == "" {
+		return nil, status.Error(codes.Unavailable, "Driver is missing version")
+	}
+
+	return &csi.GetPluginInfoResponse{
+		Name:          i.driver.name,
+		VendorVersion: i.driver.version,
+	}, nil
+}
+
+func (i *identityServer) GetPluginCapabilities(ctx context.Context, req *csi.GetPluginCapabilitiesRequest) (*csi.GetPluginCapabilitiesResponse, error) {
+	return &csi.GetPluginCapabilitiesResponse{
+		Capabilities: []*csi.PluginCapability{
+			{
+				Type: &csi.PluginCapability_Service_{
+					Service: &csi.PluginCapability_Service{
+						Type: csi.PluginCapability_Service_CONTROLLER_SERVICE,
+					},
+				},
+			},
+		},
+	}, nil
+}
+
+func (i *identityServer) Probe(ctx context.Context, req *csi.ProbeRequest) (*csi.ProbeResponse, error) {
+	return &csi.ProbeResponse{
+		Ready: &wrapperspb.BoolValue{
+			Value: true,
+		},
+	}, nil
 }
