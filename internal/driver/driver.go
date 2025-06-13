37e88cd5465d486d4b78a3f07c6ad3e977429572
Din Music <din.music@canonical.com>
2025-06-13 12:59:07 +0000

    internal/driver: Connect to devLXD and start gRPC server
    Signed-off-by: Din Music <din.music@canonical.com>

--- internal/driver/driver.go
+++ internal/driver/driver.go
@@ -1,8 +1,19 @@
 package driver
 
 import (
+	"errors"
+	"fmt"
+	"net"
+	"os"
+
 	"github.com/container-storage-interface/spec/lib/go/csi"
+	"google.golang.org/grpc"
 	"k8s.io/klog/v2"
+
+	"github.com/canonical/lxd-csi-driver/internal/devlxd"
+	"github.com/canonical/lxd-csi-driver/internal/utils"
+	lxdClient "github.com/canonical/lxd/client"
+	"github.com/canonical/lxd/shared/api"
 )
 
 // driverVersion is the version of the CSI driver.
@@ -44,7 +55,15 @@ type Driver struct {
 	nodeCapabilities       []*csi.NodeServiceCapability
 
 	// DevLXD.
+	devLXD         lxdClient.DevLXDServer
 	devLXDEndpoint string
+
+	// LXD cluster member where instance is running on.
+	location    string
+	isClustered bool
+
+	// gRPC server.
+	server *grpc.Server
 }
 
 // NewDriver initializes a new CSI driver.
@@ -60,8 +79,66 @@ func NewDriver(opts DriverOptions) *Driver {
 	return d
 }
 
-// Run starts the CSI driver.
+// Run starts CSI driver gRPC server.
 func (d *Driver) Run() error {
+	klog.InfoS("Starting LXD CSI driver",
+		"name", d.name,
+		"node", d.nodeID,
+		"version", d.version,
+	)
+
+	// Connect to devLXD.
+	devLXDClient, err := devlxd.Connect(d.devLXDEndpoint)
+	if err != nil {
+		return fmt.Errorf("Failed to connect to devLXD: %v", err)
+	}
+
+	info, err := devLXDClient.GetState()
+	if err != nil {
+		return fmt.Errorf("Failed to get LXD server info: %v", err)
+	}
+
+	// Fail early if not authenticated.
+	// In addition, this ensures we retrieve actual information whether LXD is clustered or not.
+	// If we are not authenticated, the Environment.ServerClustered field is always false.
+	if info.Auth != api.AuthTrusted {
+		return errors.New("Failed to authenticate with DevLXD server: Client is not tursted")
+	}
+
+	d.devLXD = devLXDClient
+	d.location = info.Location
+	d.isClustered = info.Environment.ServerClustered
+
+	// Construct gRPC unix address.
+	url, socket, err := utils.ParseUnixSocketURL(d.endpoint)
+	if err != nil {
+		return err
+	}
+
+	// Delete old CSI unix socket if it exists.
+	_ = os.Remove(socket)
+
+	listener, err := net.Listen("unix", socket)
+	if err != nil {
+		return fmt.Errorf("Failed to listen on %q: %v", url.String(), err)
+	}
+
+	defer func() { _ = listener.Close() }()
+
+	d.server = grpc.NewServer()
+
+	// Register CSI services.
+	csi.RegisterIdentityServer(d.server, NewIdentityServer(d))
+	csi.RegisterControllerServer(d.server, NewControllerServer(d))
+	csi.RegisterNodeServer(d.server, NewNodeServer(d))
+
+	// Start gRPC server.
+	klog.Infof("Listening for connections on address %q", url.String())
+	err = d.server.Serve(listener)
+	if err != nil {
+		return fmt.Errorf("Failed to serve gRPC server: %v", err)
+	}
+
 	return nil
 }
 
