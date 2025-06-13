70f44317504a82e2fc94d4269c36cd23a410fe6a
Din Music <din.music@canonical.com>
2025-06-13 12:20:06 +0000

    internal/drvier: Add helper functions for creating new capabilities
    Signed-off-by: Din Music <din.music@canonical.com>

---
+++ internal/driver/capabilities.go
@@ -0,0 +1,27 @@
+package driver
+
+import (
+	"github.com/container-storage-interface/spec/lib/go/csi"
+)
+
+// NewControllerServiceCapability creates a new ControllerServiceCapability.
+func NewControllerServiceCapability(cap csi.ControllerServiceCapability_RPC_Type) *csi.ControllerServiceCapability {
+	return &csi.ControllerServiceCapability{
+		Type: &csi.ControllerServiceCapability_Rpc{
+			Rpc: &csi.ControllerServiceCapability_RPC{
+				Type: cap,
+			},
+		},
+	}
+}
+
+// NewNodeServiceCapability creates a new NodeServiceCapability.
+func NewNodeServiceCapability(cap csi.NodeServiceCapability_RPC_Type) *csi.NodeServiceCapability {
+	return &csi.NodeServiceCapability{
+		Type: &csi.NodeServiceCapability_Rpc{
+			Rpc: &csi.NodeServiceCapability_RPC{
+				Type: cap,
+			},
+		},
+	}
+}
