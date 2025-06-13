89f4f65a655cf8dc18687f090dcc8dae357c6d4b
Din Music <din.music@canonical.com>
2025-06-13 14:06:41 +0000

    internal/driver: Helper function to split volume ID
    Signed-off-by: Din Music <din.music@canonical.com>

--- internal/driver/driver.go
+++ internal/driver/driver.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 
 	"github.com/container-storage-interface/spec/lib/go/csi"
 	"google.golang.org/grpc"
@@ -201,4 +202,23 @@ func getVolumeID(clusterMember string, poolName string, volName string) string {
 	}
 
 	return volumeID
+}
+
+// splitVolumeID splits an internal volume ID separated into cluster member name,
+// pool name, and volume name.
+func splitVolumeID(volumeID string) (clusterMember string, poolName string, volName string, err error) {
+	if strings.Contains(volumeID, ":") {
+		clusterMember, volumeID, _ = strings.Cut(volumeID, ":")
+	}
+
+	if volumeID == "" {
+		return "", "", "", errors.New("Volume ID is empty")
+	}
+
+	parts := strings.Split(volumeID, "/")
+	if len(parts) != 2 {
+		return "", "", "", fmt.Errorf("Invalid volume ID %q", volumeID)
+	}
+
+	return clusterMember, parts[0], parts[1], nil
 }
