d4c2113d3eb8390c5777cde26b4b5056201e29db
Din Music <din.music@canonical.com>
2025-08-18 08:33:17 +0000

    internal/driver: Identity server info
    Signed-off-by: Din Music <din.music@canonical.com>

--- internal/driver/identity.go
+++ internal/driver/identity.go
@@ -1,7 +1,11 @@
 package driver
 
 import (
+	"context"
+
 	"github.com/container-storage-interface/spec/lib/go/csi"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type identityServer struct {
@@ -16,4 +20,20 @@ func NewIdentityServer(driver *Driver) *identityServer {
 	return &identityServer{
 		driver: driver,
 	}
+}
+
+// GetPluginInfo retrieves the plugin information.
+func (i *identityServer) GetPluginInfo(ctx context.Context, req *csi.GetPluginInfoRequest) (*csi.GetPluginInfoResponse, error) {
+	if i.driver.name == "" {
+		return nil, status.Error(codes.Unavailable, "Driver is missing name")
+	}
+
+	if i.driver.version == "" {
+		return nil, status.Error(codes.Unavailable, "Driver is missing version")
+	}
+
+	return &csi.GetPluginInfoResponse{
+		Name:          i.driver.name,
+		VendorVersion: i.driver.version,
+	}, nil
 }
