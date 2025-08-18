003749567df21833468093728209abc02da6785d
Din Music <din.music@canonical.com>
2025-08-18 08:33:17 +0000

    internal/driver: Identity server probe
    Signed-off-by: Din Music <din.music@canonical.com>

--- internal/driver/identity.go
+++ internal/driver/identity.go
@@ -6,6 +6,7 @@ import (
 	"github.com/container-storage-interface/spec/lib/go/csi"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/wrapperspb"
 )
 
 type identityServer struct {
@@ -49,6 +50,16 @@ func (i *identityServer) GetPluginCapabilities(ctx context.Context, req *csi.Get
 					},
 				},
 			},
+		},
+	}, nil
+}
+
+// Probe reports plugin readiness. Always returns ready=true, since if the driver
+// were not ready the gRPC server would not have started.
+func (i *identityServer) Probe(ctx context.Context, req *csi.ProbeRequest) (*csi.ProbeResponse, error) {
+	return &csi.ProbeResponse{
+		Ready: &wrapperspb.BoolValue{
+			Value: true,
 		},
 	}, nil
 }
