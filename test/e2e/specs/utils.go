d81175e66733bf675dfd3ada5754b76ce2bed8fd
Din Music <din.music@canonical.com>
2025-09-19 06:26:44 +0000

    test/e2e/spec/utils: Helper func to print pretty resource name
    Signed-off-by: Din Music <din.music@canonical.com>

--- test/e2e/specs/utils.go
+++ test/e2e/specs/utils.go
@@ -5,6 +5,16 @@ import (
 	"time"
 )
 
+// prettyName returns a string consisting of resource's namespace and name.
+// If the namespace is empty, it returns only the name.
+func prettyName(namespace string, name string) string {
+	if namespace == "" {
+		return name
+	}
+
+	return namespace + "/" + name
+}
+
 // generateName returns a unique name composed of the given prefix, the current
 // timestamp (YYYYMMDD-hhmmss), and a 5-character random suffix.
 // Example: For input "pvc" the result is "pvc-20250818-153045-a1b2c".
