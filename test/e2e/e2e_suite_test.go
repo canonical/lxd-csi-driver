b82595cfa73a15ea9ce9565031e5ce46c0db611f
Din Music <din.music@canonical.com>
2025-09-19 06:41:58 +0000

    test/e2e: Init E2E tests and add helper function to create K8s client
    Signed-off-by: Din Music <din.music@canonical.com>

---
+++ test/e2e/e2e_suite_test.go
@@ -0,0 +1,30 @@
+package e2e
+
+import (
+	"os"
+	"testing"
+
+	"github.com/onsi/ginkgo/v2"
+	"github.com/onsi/gomega"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func TestE2e(t *testing.T) {
+	gomega.RegisterFailHandler(ginkgo.Fail)
+	ginkgo.RunSpecs(t, "E2e Suite")
+}
+
+func createClient() (*rest.Config, *kubernetes.Clientset) {
+	path := os.Getenv("K8S_KUBECONFIG_PATH")
+	gomega.Expect(path).NotTo(gomega.BeEmpty(), "K8S_KUBECONFIG_PATH environment variable must be set")
+
+	config, err := clientcmd.BuildConfigFromFlags("", path)
+	gomega.Expect(err).NotTo(gomega.HaveOccurred())
+
+	client, err := kubernetes.NewForConfig(config)
+	gomega.Expect(err).NotTo(gomega.HaveOccurred())
+
+	return config, client
+}
