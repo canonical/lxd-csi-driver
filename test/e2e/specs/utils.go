de02bf2385b3f3a9a9f0f392a8f16b4155014b14
Din Music <din.music@canonical.com>
2025-09-19 06:25:52 +0000

    test/e2e/spec/utils: Helper func to generate resource name
    Signed-off-by: Din Music <din.music@canonical.com>

---
+++ test/e2e/specs/utils.go
@@ -0,0 +1,22 @@
+package specs
+
+import (
+	"crypto/rand"
+	"time"
+)
+
+// generateName returns a unique name composed of the given prefix, the current
+// timestamp (YYYYMMDD-hhmmss), and a 5-character random suffix.
+// Example: For input "pvc" the result is "pvc-20250818-153045-a1b2c".
+func generateName(prefix string) string {
+	const letters = "abcdefghijklmnopqrstuvwxyz0123456789"
+
+	b := make([]byte, 5)
+	_, _ = rand.Read(b)
+	for i := range b {
+		b[i] = letters[int(b[i])%len(letters)]
+	}
+
+	ts := time.Now().Format("20060102-150405")
+	return prefix + "-" + ts + "-" + string(b)
+}
